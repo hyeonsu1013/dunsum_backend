1fd525d6dd1473bab1abdd2650c146896866cc9f
hyeonsu1013 <71484658+hyeonsu1013@users.noreply.github.com>
2022-11-05 23:04:04 +0900

    1. spring security 의존성 추가 2. json 의존성 추가 3. Security Config 추가 (미완)

---
+++ src/main/java/com/dunsum/backend/common/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.dunsum.backend.common.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+//    private final JwtConfigure jwtConfigure;
+
+
+
+}
