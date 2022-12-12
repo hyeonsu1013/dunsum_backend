8c5b24272a209c92da7bf252645b3c557f1490e9
hyeonsu1013 <71484658+hyeonsu1013@users.noreply.github.com>
2022-12-12 22:35:51 +0900

    1. entity 추가

---
+++ src/main/java/com/dunsum/backend/domains/entity/CodeDataEntity.java
@@ -0,0 +1,29 @@
+package com.dunsum.backend.domains.entity;
+
+import com.dunsum.backend.common.vo.BaseVO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+@ApiModel(description = "JV 데이터")
+public class CodeDataEntity extends BaseVO {
+
+    @ApiModelProperty(value = "데이터 ID")
+    private String dataId;
+
+    @ApiModelProperty(value = "데이터 ID명")
+    private String dataIdName;
+
+    @ApiModelProperty(value = "데이터 VALUE")
+    private String dataVal;
+
+    @ApiModelProperty(value = "데이터 VALUE명")
+    private String dataValName;
+
+    @ApiModelProperty(value = "데이터 설명")
+    private String dataDesc;
+
+}
---
+++ src/main/java/com/dunsum/backend/domains/entity/UserCharEntity.java
@@ -0,0 +1,32 @@
+package com.dunsum.backend.domains.entity;
+
+import com.dunsum.backend.common.vo.BaseVO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+@ApiModel(description = "사용인별 캐릭터 등록")
+public class UserCharEntity extends BaseVO {
+
+    @ApiModelProperty(value = "사용인번호")
+    private long userNo;
+
+    @ApiModelProperty(value = "서버 ID")
+    private String srvrId;
+
+    @ApiModelProperty(value = "캐릭터 ID")
+    private String charId;
+
+    @ApiModelProperty(value = "등록실패횟수")
+    private int failCnt;
+
+    @ApiModelProperty(value = "등록실패일자")
+    private String failDate;
+
+    @ApiModelProperty(value = "DB 상태")
+    private String dbStat;
+
+}
